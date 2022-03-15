28ba8f5ba6ffa60dcb1b9feceef831cfb1dc567e
Mozadded-Ul-Islam <pmozadded@gmail.com>
2022-03-15 09:22:08 +0600

    one last time

--- src/components/Faculty/Faculty.js
+++ src/components/Faculty/Faculty.js
@@ -10,7 +10,7 @@ import { TeacherContext } from '../../App';
 
 const Faculty = () => {
 
-    const [teaher, setTeacher] = useState([])
+    const [teacher, setTeacher] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:4000/teachers')
@@ -23,21 +23,23 @@ const Faculty = () => {
             <div className="team">
                 <div className="container">
                     <h1>OUR FACULTY</h1>
-                    <div className="row">
-                        {
-                            teacher.map(teacher =>
+                    {
+                        teacher.map(teacher => 
+                            <div className="row">
                                 <div className="col-md-3">
                                     <div className="card board border-primary mb-3">
                                         <div className="card-header">{teacher.teacherData.name}</div>
                                         <div className="card-body">
-                                            <h5 className="card-title">{teacher.teacherData.Designation}</h5>
-                                            <p className="card-text">{teacher.teacherData.Description}</p>
+                                            <h5 className="card-title">{teacher.teacherData.designation}</h5>
+                                            <p className="card-text">{teacher.teacherData.description}</p>
                                         </div>
                                         <div className="card-footer bg-transparent border-primary">Contact Info</div>
                                     </div>
                                 </div>
-}
                     </div>
+                            )
+                    }
+                    
                 </div>
 
             </div>
